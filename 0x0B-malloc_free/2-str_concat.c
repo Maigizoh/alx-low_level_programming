4086b86107e8830e1a5a29a8799657dccffe64e9
root <mmaigizoh@gmail.com>
2022-10-05 03:36:29 -0700

    commit 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,34 +5,34 @@
 *@s1:First string
 *@s2:Second string
 *
-*Return: NULL in case of failure , but pointer to a new string in
+*Return: NULL in case of failure , but pointer to new string in
 *case of success
 */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *str_concat;
-	int index, concat_index = 0, len = 0;
+	char *concat_str;
+	int index, concat_index = 0,  len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
-		s1 = "";
+		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 
-	str_concat = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (str_concat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		str_concat[concat_index++] = s1[index];
+		concat_str[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		str_concat[concat_index++] = s2[index];
+		concat_str[concat_index++] = s2[index];
 
-	return (str_concat);
+	return (concat_str);
 }
